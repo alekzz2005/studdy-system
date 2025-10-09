e5be6c4766d8925721b5c2357f89938074fa8db9
therean-07 <johntiklo7@gmail.com>
2025-10-09 20:22:21 +0800

    Created the project and added the necessary packages

---
+++ besanezg5/src/test/java/com/appdev/cjainnovators/besanezg5/Besanezg5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.cjainnovators.besanezg5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Besanezg5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
